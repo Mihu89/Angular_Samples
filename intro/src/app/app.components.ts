a0efbdb4dc224cdf41782c20850178b5a3795769
mihail.coscodan <mihail.coscodan@dassolutions.eu>
2020-07-28 20:12:47 +0300

    First app is ready

--- intro/src/app/app.components.ts
+++ intro/src/app/app.components.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<label>Write your user name</label>
+    template: `<label>Write your user name</label>&nbsp;
     <input type="text" placeholder="name" [(ngModel)]="name">
     <h1 style="background-color: green;">Hello {{name}} !</h1>`
 })
--- intro/src/main.ts
+++ intro/src/main.ts
@@ -1,2 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule }
+import { AppModule } from './app/app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
